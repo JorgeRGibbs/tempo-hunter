45aab3a45156d642ef788c5bb61d236053aadab8
JorgeRGibbs <jorgegibbs@outlook.com>
2020-02-03 14:05:55 -0600

    Added code

---
+++ access.py
@@ -0,0 +1,4 @@
+import spotipy 
+import cred
+
+util.prompt_for_user_token(username,scope,client_id=id(),client_secret=secret(),redirect_uri='http://localhost/')
---
+++ cred.py
@@ -0,0 +1,8 @@
+
+
+def id():
+	client_id = '7faa6cc462b84783b70ed75d10067b47'
+	return client_id
+def secret():
+	client_secret = '95e2712f0fb24f989cf85793c3e0eca1'
+	return client_secret
